1b7c1bea0ba4452c055fab92c296c8debe0a9df5
Shama Abro <shamaabro786@gmail.com>
2020-06-12 12:21:24 +0500

    First Commit

---
+++ app.js
@@ -0,0 +1,127 @@
+//  Chapter #1
+//  Task #1 and #2
+
+// var a ="Error!  ";
+// var b = "Please enter a valid password";
+// var c = a + b;
+// alert (c);
+
+//  Task #3
+
+//   var a ="Welcome to JS Land...";
+//   var b = "Happy Coding"; 
+//   var c = a+"\n" + b; 
+//    alert (c );
+
+//  Task #4
+
+//     var a ="Welcome to JS Land...";
+//     alert (a);
+//    var b = "Happy Coding";
+//    var c = "Prevent this page from creating additional dialogs.";
+//    var d = b+"\n" +c;
+//    alert (d);
+
+// Task #5
+
+// var a = "Hello...";
+// var b = "I can run JS through my web browser's console ";
+// var c = a + b;
+// alert (c)
+// console.log (c)
+
+// Chapter #2
+
+// Task #1
+
+// var a = username;
+
+// Task #2
+
+// var b = "Sunbal"
+
+// Task #3
+
+// var message = "Hello World";
+// alert (message);
+
+// Task #4
+
+// var a = "Sunbal "
+// var b = "16 years old"
+// var c = "Certified Mobile Application Development"
+
+// alert (a)
+// alert (b)
+// alert (c)
+
+
+// Task #5
+
+// var a = "PIZZA";
+// alert (a+"\n"+"PIZZ"+"\n"+"PIZ"+"\n"+"PI"+"\n"+"P");
+   
+
+// Task #6
+
+// var email = "my email address is sunbalabro786@gmail.com";
+// alert(email);
+
+
+// Task #7
+
+
+// var book = "I am trying to learn from the Book A smarter way to learn JavaScript";
+
+// alert (book);
+
+
+// Task #8
+
+// document.write ("Yah! I can write HTML content through JavaScript");
+
+
+// Task #9
+
+// var a = "▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+
+// alert (a);
+
+
+
+// CHAPTER # 3
+
+
+// Task #1
+
+// var age = "I am 16  years old";
+// alert(age);
+
+// Task #2
+
+
+
+
+
+// Task #3
+
+// var birthyear = "My birth year is 2004";
+// document.write (birthyear+"<br>"+"Data type of my decleared variable is number");
+
+// Task #4 
+
+// var data = "John Doe ordered 5 T-shirt(s) on XYZ Clothing store";
+
+// document.write (data);
+
+
+
+// CHAPTER #4
+
+// Task #1
+
+
+
+
+
+
